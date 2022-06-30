2320f058144698c6b7c7ffdfd85c5b062681afda
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2022-06-30 07:24:51 -0300

    Include button
    effect in this button

--- src/Component/Head/Header.js
+++ src/Component/Head/Header.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="container d_flex">
-                <div className="logo"></div>
-                <img src="" alt="" />
-            </div>
-            <div className="navlink">
-                <ul className="link f_flex uppercase">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#features">features</a></li>
-                    <li><a href="#portfolio">portfolio</a></li>
-                    <li><a href="#resume">resume</a></li>
-                    <li><a href="#clients">clients</a></li>
-                    <li><a href="#blog">blog</a></li>
-                    <li><a href="#contact">contact</a></li>
-                </ul>
-                <button className="toggle"></button>
-            </div>
 
-        </header >
+    window.addEventListener("scrool", function () {
+        const header = document.querySelector(".header")
+        header.classList.toggle("active", window.scroolY > 100)
+    })
+
+    const [Mobile, setMobile] = useState(false)
+    return (
+        <>
+            <header className="header">
+                <div className="container d_flex">
+                    <div className="logo"></div>
+                    <img src="" alt="" />
+                </div>
+                <div className="navlink">
+                    {/*<ul className="link f_flex uppercase">*/}
+                    <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#features">features</a></li>
+                        <li><a href="#portfolio">portfolio</a></li>
+                        <li><a href="#resume">resume</a></li>
+                        <li><a href="#clients">clients</a></li>
+                        <li><a href="#blog">blog</a></li>
+                        <li><a href="#contact">contact</a></li>
+                        <li><button className='home-btn'>Clique aqui</button>
+                        </li>
+                    </ul>
+                    <button className="toggle" onClick={() => setMobile(Mobile)}>
+                        {Mobile ?
+                            <i className="fas fa-times close home-btn"></i> :
+                            <i className="fas fa-bars open"></i>
+                        }
+                    </button>
+                </div>
+            </header >
+        </>
     )
 }
 
